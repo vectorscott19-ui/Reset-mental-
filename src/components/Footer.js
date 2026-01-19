7035f11f771f8594a9ea5b822d1504bb87e1ce4e
vectorscott19-ui <vectorscott19@gmail.com>
2026-01-19 22:21:23 +0200

    Add Footer component with copyright text

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <p style={{ marginTop: 40, fontSize: 12, opacity: 0.6 }}>
+      Dia Consciente ©
+    </p>
+  );
+}
+
+export default Footer;
